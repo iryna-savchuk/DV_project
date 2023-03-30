c8de45a0fdff10558fe6ba8d96434d9982873556
Iryna S <iryna.o.savchuk@gmail.com>
2023-03-30 19:23:56 +0100

    added chorplet map and slider

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import dcc
 from dash import html
+import dash_daq as daq
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
@@ -16,15 +17,42 @@ df = pd.read_csv(path + 'merged.csv')
 #### Building Graphs ######
 ###########################
 
-# Age 
-x = df['prizeAge']
-hist_data = dict(type='histogram', x=x, marker=dict(color='silver'))
-layout = dict(title=dict(text='Ages Distribution'))
-fig_1 = go.Figure(data=hist_data, layout=layout)
+#### Age Histogram
+data_hist_age = dict(type='histogram', 
+                     x=df['prizeAge'], 
+                     marker=dict(color='#a57a50'),
+                     hovertemplate='Age when won Prize: %{x} <br>'+'Number od winners: %{y}<br><extra></extra>')
+
+layout_hist_age = dict(title=dict(text='Ages Distribution'),
+                   plot_bgcolor='#fcf2bf')
+
+fig_hist_age = go.Figure(data=data_hist_age, layout=layout_hist_age)
+fig_hist_age.update_yaxes(showline=True, linewidth=2, linecolor='#674e04', gridcolor='#a57a50')
+fig_hist_age.update_xaxes(showline=True, linewidth=2, linecolor='#674e04')
 
 
+#### Category Barchart
+category_labels = df['category'].value_counts()
+category_values = (category_labels / category_labels.sum()) * 100
+unique_category = df['category'].unique()
 
-# Choropleth
+data_bar_category = dict(type='bar',
+                        x=unique_category,
+                        y=category_values,
+                        marker=dict(color=[ '#623f17', '#ab6400', '#eb993c',  '#a57a50', '#e4a76c','#877769', '#a58a89' ]),
+                        hovertemplate='Category: %{x} <br>'+'Percentage: %{y}<br><extra></extra>'
+                        )
+
+layout_bar_category = dict(title=dict(text='Prizes by Category'), 
+                           xaxis=dict(title='category'), 
+                           yaxis=dict(title='Percentage'),
+                           plot_bgcolor='#fcf2bf'
+                  )
+
+fig_bar_category = go.Figure(data=[data_bar_category], layout=layout_bar_category)
+
+
+#### Choropleth
 df_density = make_density_df(df)
 
 data_choropleth = dict(type='choropleth',
@@ -33,21 +61,36 @@ data_choropleth = dict(type='choropleth',
                         z=np.log(df_density['count']),
                         zmin=np.log(df_density['count'].min()),
                         zmax=np.log(df_density['count'].max()),
-                        colorscale = ["#fcf2bf", "#ab8206"],   
+                        colorscale = ["#fcf2bf", "#ab6400"],   
                         marker_line_color = '#674e04',
                         colorbar=dict(title='Total Prizes(log)'),
                         text=df_density['name'],
-                        hovertemplate='Country: %{text} <br>'+'Prizes (log): %{z} <br><extra></extra>'
+                        hovertemplate='Country: %{text} <br>'+'Prizes (log): %{z} <br><extra></extra>',
                         )
                     
-layout_choropleth = dict(geo=dict(projection={'type': 'natural earth'}, bgcolor= 'rgba(0,0,0,0)'))
+layout_choropleth = dict(geo=dict(projection={'type': 'natural earth'}, 
+                         bgcolor= 'rgba(0,0,0,0)'))
 
 fig_choropleth = go.Figure(data=data_choropleth, layout=layout_choropleth)
+fig_choropleth.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
+#### Slider Map
+slider_map = daq.Slider(
+        id = 'slider_map',
+        handleLabel={"showCurrentValue": True, "label": "Year"},
+        marks = {str(i):str(i) for i in range(1900, 2020, 10)},
+        min = 1900,
+        max = 2022,
+        value = 2022, 
+        #step = 1,
+        size = 1000,
+        vertical=True,
+        color='#ab6400'
+    )
 
-###################
-#### The APP ######
-###################
+##########################
+#### The APP Layout ######
+##########################
 app = dash.Dash(__name__)
 server = app.server
 
@@ -118,32 +161,34 @@ app.layout =  html.Div([
 
         html.Div(
             [
-                html.H6("General health information about the countries", style={"margin-top": "0","font-weight": "bold","text-align": "center"}),
-                html.P("Similarly to nutrition, the health status also varies from country to country. The bar chart below shows the differences between the countries in terms of the following variables: prevalence of obesity in the adult population in % (Obesity), prevalence of diabetes in the adult population in % (Diabetes Prevalence), cardiovascular death rate per 100,000 population (Cardiovascular Death Rate), average life expectancy in years (Life Expectancy) and the expenditure of the government on the country's health system in % of the respective GDP (Health Expenditure).", className="control_label",style={"text-align": "justify"}),
-                
-                html.Div([dcc.Graph(id="bar_chart_category", figure=fig_1)],className="pretty_container twelve columns"),
+                html.H6("General Nobel Prize information", style={"margin-top": "0","font-weight": "bold","text-align": "center"}),
+                html.P("The Nobel Prize is an international award administered by the Nobel Foundation in Stockholm, Sweden, and based on the fortune of Alfred Nobel, Swedish inventor and entrepreneur. In 1968, Sveriges Riksbank established The Sveriges Riksbank Prize in Economic Sciences in Memory of Alfred Nobel, founder of the Nobel Prize. Each prize consists of a medal, a personal diploma, and a cash award.", 
+                    className="control_label",style={"text-align": "justify"}),
+                html.P("A person or organisation awarded the Nobel Prize is called Nobel Prize laureate. The word “laureate” refers to being signified by the laurel wreath. In ancient Greece, laurel wreaths were awarded to victors as a sign of honour.", 
+                    className="control_label",style={"text-align": "justify"}),
+                html.Div([dcc.Graph(id="fig_hist_age", figure=fig_hist_age)], className="pretty_container five columns"),
+                html.Div([dcc.Graph(id="fig_bar_category", figure=fig_bar_category)], className="pretty_container five columns"),
             ],
             className="row pretty_container",
         ),
 
         html.Div([
-            html.H1(children='Nobel Prizes Dashboard'),
-            html.Label('We are interested in investigating the historucal data on Nobel Prizes laureates.', 
-                        style={'color':'rgb(33 36 35)'}), 
-            html.Img(src=app.get_asset_url('Nobel_Prize.png'), style={'position': 'relative'}),
-            ]
+            html.H6("Nobel Prizes Distribution by Country", style={"margin-top": "0","font-weight": "bold","text-align": "center"}), 
+            html.Div(
+                [dcc.Graph( id='choropleth-graph', figure=fig_choropleth)],
+                className="row pretty_container",
+            ),
+            html.Div(style={'margin-top': 50}), 
+            dcc.RangeSlider(min=1900, max=2022, value=[1901, 2022], 
+                            marks={ 1901: '1901', 1910: '1910', 1920: '1920',  1930: '1930',
+                                    1940: '1940', 1950: '1950', 1960: '1960',  1970: '1970',
+                                    1980: '1980', 1990: '1990', 2000: '2000',  2010: '2010',
+                                    2020: '2020',},
+                            tooltip={"always_visible": True}, 
+                            )
+            ],
+            className="row pretty_container",
         ),
-
-        html.Div(children=[
-            html.H1(children='Nobel Prizes Dashboard'),
-
-            html.Div(children='''Map'''),
-
-            dcc.Graph(
-                id='choropleth-graph',
-                figure=fig_choropleth
-            )
-        ])
     ]), # Main Body Div --end
 
 
@@ -177,6 +222,11 @@ app.layout =  html.Div([
             )
     ]) # Footer Div --end
 ]) # app layout Div --end
+
+
+#####################
+#### Callbacks ######
+#####################
 
 
 if __name__ == '__main__':
